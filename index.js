825dc326a29621f6ada00edf46966494e7073834
BSK <bs.karthik@gmail.com>
2023-12-28 15:06:30 +0530

    all links open in the same window

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ document.addEventListener("DOMContentLoaded", function() {
     var listItems = document.querySelectorAll('li');
 
     listItems.forEach(function(item) {
-        item.addEventListener('click', function() {
-            // Remove the 'active' class from all list items
-            listItems.forEach(function(li) {
-                li.classList.remove('active');
-            });
-
-            // Add the 'active' class to the clicked list item
-            this.classList.add('active');
-            this.style.color = '#087ea4';
+      item.addEventListener('click', function() {
+        // Remove the 'active' class from all list items
+        listItems.forEach(function(li) {
+          li.classList.remove('active');
         });
+
+        // Add the 'active' class to the clicked list item
+        this.classList.add('active');
+      });
     });
-});
+  });
